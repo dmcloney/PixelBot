fa45986c527ea1597bc22e050ea3a39e2f2f9c2d
PixelGalaxxyy <dddash.dm@gmail.com>
2021-07-26 02:16:32 -0400

    Revert "Added partial PCRP command, also included PCRP IP File"
    This reverts commit 8915ff1407f29be04f68dc31ee18ee0647f4bf6e.

--- bot.js
+++ bot.js
@@ -6,10 +6,11 @@ const prefix = '!';
 
 // CF - This section is to allow seperate command files
 const fs = require('fs');
+
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	console.log('Loaded File: ' + file);
+	console.log(file);
 	const command = require(`./commands/${file}`);
 
 	client.commands.set(command.name, command);
@@ -17,31 +18,64 @@ for (const file of commandFiles) {
 //End of Section
 
 var auth = require('./auth.json');
+//var seconds = 5;
+//const app = prefix();
+//app.use(helmet());
+//app.use(morgan('dev'));
 
 //Setting Bot information
 client.login(auth.token);
 client.on('ready', () => {
-	console.log(`--------------------------------`);
 	console.log(`Logged in as ${client.user.tag}!`);
 	console.log(`Client ID is ${client.user.id}`);
-	console.log(`--------------------------------`);
 	console.log('Bot Launched...');
 	client.user.setActivity("on Pixel's Computer", { type: 'STREAMING', url: 'https://twitch.tv/pixelgalaxxyy' }); //Telling the bot to say it is streaming
-	// API Section, not used right now
-	//client.api.applications(client.user.id).commands.post({data: {
-	//	name: 'ping',
-	//	description: 'ping pong!'
-	//}});
-	//
+//
+	client.api.applications(client.user.id).commands.post({data: {
+		name: 'ping',
+		description: 'ping pong!'
+	}});
+//
 });
 
 
-client.on('message', msg => {
+client.on( 'message', msg => {
 	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
 	const commandBody = msg.content.slice(prefix.length);
 	const args = commandBody.split(' ');
 	const command = args.shift().toLowerCase();
-
+	//const botchannel = '697975823415902278';
+	/*if ( command === 'pingg' ) {
+		client.commands.get('ping').execute(msg, args);
+	};
+	if (msg.content === prefix + 'delete') {
+			msg.reply('Closing channel in 5 seconds!').then((msg) => {
+				for (let i = 5; i >=0; i--) {
+					setTimeout(() => {
+						msg.edit(`Closing channel in ${i} seconds!`);
+					}, 5000)
+				}
+			});
+		};
+	//This is the radd command
+	if (command === 'radd') {
+		client.commands.get('radd').execute(msg, args);
+	};*/
 	client.commands.get(`${command}`).execute(msg, args);
+	// if (command === 'awesome') {
+	// 	client.commands.get('awesome').execute(msg, args);
+	// };
+	// if (command === 'roles') {
+	// 	client.commands.get('roles').execute(msg, args);
+	// };
+	// if (command === 'startmsg') {
+	// 	client.commands.get('startmsg').execute(msg, args);
+	// };
+	// if (command === 'reactions') {
+	// 	client.commands.get('reactions').execute(msg, args);
+	// };
+	// if (command === 'projectcityrp') {
+	// 	client.commands.get('projectcityrp').execute(msg, args);
+	// };
 });
---
+++ commands/projectcityrp.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const fivem = require("discord-fivem-api");
+const server = new fivem.DiscordFivemApi("51.81.166.101:30120");
+module.exports = {
+    name: 'projectcityrp',
+    description: "For the FiveM server",
+
+    async execute(msg, args) {
+        server.getPlayersOnline().then((res) => {
+            msg.channel.send("Connected to the Project City RP Server, Players Online: " + `${res}`).catch(console.error);
+            console.log("Connected to the Project City RP Server, Players Online: " + res)
+        });
+    }
+}
